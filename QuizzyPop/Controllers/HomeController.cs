6449cae603cb70a785cd3e5ea51a14b8cba62d87
vincenrepeat <viiinceeee.nt27@gmail.com>
2025-09-22 22:30:20 +0200

    Some more smaller corrections

--- QuizzyPop/Controllers/HomeController.cs
+++ QuizzyPop/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult CreateQuiz()
+     public IActionResult CreateQuiz()
     {
         return View();
     }

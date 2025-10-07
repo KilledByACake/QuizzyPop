88ff1f6e5d785f2953f403aba172b480b3c524c3
Anja Trbos <antrb3968@oslomet.no>
2025-10-07 21:03:10 +0200

    Implement login and registration views with responsive design and improved styling

---
+++ QuizzyPop/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuizzyPop.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
+
+

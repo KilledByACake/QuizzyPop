c41e0354ccf28618e01bf11d65ce870816325f75
vincenrepeat <viiinceeee.nt27@gmail.com>
2025-10-12 16:35:29 +0200

    Expanded Database

--- QuizzyPop/Migrations/20251012143454_UserDbExpanded.Designer.cs
+++ QuizzyPop/Migrations/20251012143454_UserDbExpanded.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QuizzyPop.Models;
 
@@ -10,9 +11,11 @@ using QuizzyPop.Models;
 namespace QuizzyPop.Migrations
 {
     [DbContext(typeof(UserDbContext))]
-    partial class UserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251012143454_UserDbExpanded")]
+    partial class UserDbExpanded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.20");
